6d3a0e4b9d64049bd03e796abfbe5a54a9e23a07
Lucas Martin Luzzani <lucasluzzani@gmail.com>
2022-06-26 13:47:41 -0300

    testimonios y portfolio agregados

--- src/components/testimonials/Testimonials.jsx
+++ src/components/testimonials/Testimonials.jsx
@@ -1,8 +1,71 @@
 import React from "react";
 import "./testimonials.css";
+import AVATAR from "../../assets/avatar.jpg";
+
+// import Swipper core and required module 
+import { Pagination } from 'swiper';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper Styles 
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+
+const data = [
+  {
+    avatar: AVATAR,
+    name: 'John Doe',
+    review: 'Explicabo, expedita odit blanditiis amet tempora excepturi accusantium sit eligendi repudiandae? Fugit necessitatibus ut laboriosam hic sit quam consequuntur praesentium, molestias sed!'
+  },
+  {
+    avatar: AVATAR,
+    name: 'John Doe',
+    review: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Explicabo, expedita odit blanditiis amet tempora excepturi accusantium sit eligendi repudiandae? Fugit necessitatibus ut laboriosam hic sit quam consequuntur praesentium, molestias sed!'
+  },
+  {
+    avatar: AVATAR,
+    name: 'John Doe',
+    review: 'dolor sit amet consectetur adipisicing elit. Explicabo, expedita odit blanditiis amet tempora excepturi accusantium sit eligendi repudiandae? Fugit nec praesentium, molestias sed!'
+  },
+  {
+    avatar: AVATAR,
+    name: 'John Doe',
+    review: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Fugit necessitatibus ut laboriosam hic sit quam consequuntur praesentium, molestias sed!'
+  },
+]
 
 const Testimonials = () => {
-  return <div>Testimonials</div>;
+  return (
+    <section id="testimonials">
+      <h5>Review from clients</h5>
+      <h2>Testimonials</h2>
+
+      <Swiper className="container testimonials__container"
+      // install Swiper modules
+      modules={[Pagination]}
+      spaceBetween={40}
+      slidesPerView={1}
+      pagination={{clickable: true}}
+      >
+      {
+        data.map(({avatar, name, review}, index) => {
+          return(
+            <SwiperSlide keyy={index} className="testimonial">
+            <div className="client__avatar">
+              <img src={avatar} alt="client avatar" />
+            </div>
+              <h5 className="client__name">{name}</h5>
+              <small className="client__review">
+                {review}
+              </small>
+          </SwiperSlide>
+          )
+        })
+      }
+      </Swiper>
+    </section>
+  );
 };
 
 export default Testimonials;
